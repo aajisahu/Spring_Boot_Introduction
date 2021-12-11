f4f21c1b227dc78669e57f2b863ea9ba87fa910d
aajisahu <aajisahu@gmail.com>
2021-12-11 18:00:29 +0530

    Ability to create HelloWorld Spring App

---
+++ src/main/java/com/springintroduction/SpringIntroductionApplication.java
@@ -0,0 +1,14 @@
+package com.springintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroductionApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello! From BridgeLabz");
+        SpringApplication.run(SpringIntroductionApplication.class, args);
+    }
+
+}
